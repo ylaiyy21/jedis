09d3dc39f431145f1e118fefcd0fcaef8f037f44
ylaiyy21 <827444827@qq.com>
2016-10-09 16:14:16 +0800

    初始化

---
+++ src/com/jedis/pojo/Person.java
@@ -0,0 +1,30 @@
+package com.jedis.pojo;
+
+public class Person {
+
+	private String code;
+	private String name;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String toString(){
+		
+		return code+","+name;
+	}
+	
+	public String getStrings(String... s){
+		
+		return s[0];
+	}
+}
---
+++ src/com/jedis/test/Test.java
@@ -0,0 +1,70 @@
+package com.jedis.test;
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.jedis.pojo.Person;
+
+import redis.clients.jedis.Jedis;
+
+
+
+public class Test {
+	private Jedis jedis;
+	
+	@Before
+	public void initJedis(){
+		jedis = new Jedis("127.0.0.1",6379);
+		
+	}
+	
+	/**
+	 * redis设置字符串
+	 */
+	@org.junit.Test 
+	public void test1(){
+		jedis.set("name", "tom");
+		System.out.println("set:"+jedis.get("name"));
+		jedis.del("name");
+		System.out.println("del:"+jedis.get("name"));
+		jedis.append("name", ",jack");
+		System.out.println("append:"+jedis.get("name"));
+		jedis.mset("code","123","name","jose","pass","qwe","age","11");
+		System.out.println("code:" + jedis.get("code") + ",name:" 
+				+ jedis.get("name") + ",pass:" + jedis.get("pass"));
+		jedis.incr("age");
+		System.out.println("incr:"+jedis.get("age"));
+		
+	}
+	
+	/**
+	 * redis设置map对象
+	 */
+	
+	@org.junit.Test
+	public void test2(){
+		Map<String, String> user = new HashMap<String, String>();
+		user.put("code", "1245");
+		user.put("name", "lose");
+		jedis.hmset("user",user);
+		
+		Iterator<E>
+		
+	}
+	
+	/**
+	 * 指定类型不确定参数
+	 */
+	@org.junit.Test
+	public void test3(){
+		Person person = new Person();
+		System.out.println(person.getStrings("3333333","44444444"));
+		System.out.println(person.getStrings("1111","22222","3333333","44444444"));
+	}
+}
